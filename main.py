c9d0a07fc148eaccbb14d35da07758d0e4fe7108
hanseul <w0w1278@naver.com>
2022-06-14 14:14:01 +0900

    practice for tesseract

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+# encoding: utf-8
+from pytesseract import *
+from PIL import Image
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+image = Image.open("C:\\Users\\w0w12\\Desktop\\hs_주민등록증_for_data1.jpg")
 
+print01 = image_to_string(image, lang='kor',config ='--psm 12 -c preserve_interword_spaces=1')
+print(print01)
+# print(pt.image_to_string(image,lang='kor',config= '-c preserve_interword_spaces=1 --psm 4'))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ wordmk.py
@@ -0,0 +1,16 @@
+# encoding: utf-8
+from docx import Document
+
+doc = Document()
+
+table = doc.add_table(rows = 1, cols=4)
+table.style = doc.styles['Table Grid']
+
+first_row = table.rows[0].cells
+
+first_row[0].text = '이름'
+first_row[1].text = '생년월일'
+first_row[2].text = '주소'
+first_row[3].text = '업로드 된 시간'
+
+doc.save('ImageInformation.docx')
