f62989ef5fe04180df86a6bc45eec9402d2e93a8
hanseul <w0w1278@naver.com>
2022-06-29 16:11:56 +0900

    edit

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from imutils.perspective import four_point_transform
 from imutils.contours import sort_contours
 import imutils
 import re
-
+from datetime import datetime
 #이미지 불러오기
 #image = Image.open("C:\\Users\\w0w12\\Desktop\\hs_주민등록증_for_data1.jpg")
 #image_nparray = np.asarray("C:/Users/w0w12/Desktop/hs_주민등록증_for_data1.jpg",dtype= np.uint8)
-image = cv2.imread(r"C:\Users\w0w12\Desktop\hs_for_data.jpg",cv2.IMREAD_COLOR)
+image = cv2.imread(r"C:\Users\w0w12\Desktop\data2.jpg",cv2.IMREAD_COLOR)
 #print(image_nparray.shape)
 # print(image.shape)
 # 이미지 전처리
@@ -54,9 +54,10 @@ def scan_img(image, width,ksize=(3,3), min_threshold=100, max_threshold=200):
 image_scan = scan_img(image,width=200,ksize=(3,3), min_threshold=180,max_threshold=210)
 # 전처리한 이미지로 텍스트 추출
 text = image_to_string(image_scan, lang='kor',config ='--psm 1 -c preserve_interword_spaces=1')
-text = re.sub("\d+\s +\d","",text)
-text.replace("/\d+\s+\d","")
+text = re.sub("\s\d\s","",text)
+
 print(text)
+
 # # print(pt.image_to_string(image,lang='kor',config= '-c preserve_interword_spaces=1 --psm 4'))
 # print(text[2])
 #추출한 텍스트 word에 올리기
@@ -64,29 +65,30 @@ name = re.findall(r"\w{3}",text)[1]
 #birthdate = re.findall(r"\d{6}", text)
 birthdate = re.findall(r'[\+\(]?[1-9][0-9 ]{8,}[0-9]', text)
 address = re.findall(r"\w+시\b \w+구\b",text)[0]
-# date =
+date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 print(name)
 print(birthdate)
 print(address)
+print(date)
 
-# from docx import Document
-#
-# doc = Document()
-#
-# table = doc.add_table(rows = 1, cols=4)
-# table.style = doc.styles['Table Grid']
-#
-# first_row = table.rows[0].cells
-#
-# first_row[0].text = '이름'
-# first_row[1].text = '생년월일'
-# first_row[2].text = '주소'
-# first_row[3].text = '업로드 된 시간'
-#
-# row_cells = table.add_row().cells
-# row_cells[0].text = name
-# row_cells[1].text = birthdate
-# row_cells[2].text = address
-#
-# doc.save('ImageInformation.docx')
+from docx import Document
+
+doc = Document()
+
+table = doc.add_table(rows = 1, cols=4)
+table.style = doc.styles['Table Grid']
+
+first_row = table.rows[0].cells
+
+first_row[0].text = '이름'
+first_row[1].text = '생년월일'
+first_row[2].text = '주소'
+first_row[3].text = '업로드 된 시간'
+
+row_cells = table.add_row().cells
+row_cells[0].text = name
+row_cells[1].text = '0'
+row_cells[2].text = address
+row_cells[3].text = date
+doc.save('ImageInformation.docx')
