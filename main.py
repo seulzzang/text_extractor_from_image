4bf6955f9e032f004e005222ff975a826edd1cfe
hanseul <w0w1278@naver.com>
2022-06-29 14:46:15 +0900

    extractor

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 # encoding: utf-8
 from pytesseract import *
-from PIL import Image
 import cv2
 import numpy as np
 from imutils.perspective import four_point_transform
 from imutils.contours import sort_contours
 import imutils
+import re
 
 #이미지 불러오기
 #image = Image.open("C:\\Users\\w0w12\\Desktop\\hs_주민등록증_for_data1.jpg")
 #image_nparray = np.asarray("C:/Users/w0w12/Desktop/hs_주민등록증_for_data1.jpg",dtype= np.uint8)
-image = cv2.imread(r"C:\Users\w0w12\Desktop\data2.jpg",cv2.IMREAD_COLOR)
+image = cv2.imread(r"C:\Users\w0w12\Desktop\hs_for_data.jpg",cv2.IMREAD_COLOR)
 #print(image_nparray.shape)
 # print(image.shape)
 # 이미지 전처리
 ## 첫 큰 사각형을 주민등록증 외곽으로 판단
 ## 주민등록증 외곽을 기준으로 이미지 보정
-def scan_img(image, width,ksize=(1,1), min_threshold=190, max_threshold=210):
+def scan_img(image, width,ksize=(3,3), min_threshold=100, max_threshold=200):
     org_img = image.copy()
     image = imutils.resize(image, width=width)
     ratio = org_img.shape[1] / float(image.shape[1])
 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # gray = cv2.normalize(gray, None, 0, 255, cv2.NORM_MINMAX)
-    chahe = cv2.createCLAHE(clipLimit=20.0, tileGridSize=(1,1))
+    gray = cv2.normalize(gray, None, 0, 255, cv2.NORM_MINMAX)
+    chahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
     gray = chahe.apply(gray)
 
     blurred = cv2.GaussianBlur(gray, ksize, 0)
@@ -51,10 +51,39 @@ def scan_img(image, width,ksize=(1,1), min_threshold=190, max_threshold=210):
 
 
 
-image_scan = scan_img(image,width=200,ksize=(1,1), min_threshold=180,max_threshold=210)
+image_scan = scan_img(image,width=200,ksize=(3,3), min_threshold=180,max_threshold=210)
 # 전처리한 이미지로 텍스트 추출
-print01 = image_to_string(image_scan, lang='kor',config ='--psm 1 -c preserve_interword_spaces=1')
-print(print01)
+text = image_to_string(image_scan, lang='kor',config ='--psm 1 -c preserve_interword_spaces=1')
+text = text.replace(" ","")
+print(text)
 # # print(pt.image_to_string(image,lang='kor',config= '-c preserve_interword_spaces=1 --psm 4'))
 
-#추출한 텍스트 word에 올리기 : wordmk.py
+#추출한 텍스트 word에 올리기
+name = re.findall(r"\w{3}",text)[1]
+birthdate = re.findall(r"\d{6}", text)[0]
+address = re.findall(r"\w{3}(\w+구\b)",text)
+# date =
+print(name)
+print(birthdate)
+print(address)
+
+# from docx import Document
+#
+# doc = Document()
+#
+# table = doc.add_table(rows = 1, cols=4)
+# table.style = doc.styles['Table Grid']
+#
+# first_row = table.rows[0].cells
+#
+# first_row[0].text = '이름'
+# first_row[1].text = '생년월일'
+# first_row[2].text = '주소'
+# first_row[3].text = '업로드 된 시간'
+#
+# row_cells = table.add_row().cells
+# row_cells[0].text = name
+# row_cells[1].text = birthdate
+# row_cells[2].text = address
+#
+# doc.save('ImageInformation.docx')
